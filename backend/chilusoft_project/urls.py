79ec3b2478a5d152dc4e709877cfdc5fa28f32ea
chilusoft <mukukchilu@gmail.com>
2023-04-11 01:13:09 +0200

    Added JWT authentication over REST API

--- backend/chilusoft_project/urls.py
+++ backend/chilusoft_project/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('token/', 
+    path('api/v1/', include('custom_auth.urls')),
+    path('api/v1/token/', 
           jwt_views.TokenObtainPairView.as_view(), 
           name ='token_obtain_pair'),
-     path('token/refresh/', 
+     path('api/v1/token/refresh/', 
           jwt_views.TokenRefreshView.as_view(), 
           name ='token_refresh')
 ]
