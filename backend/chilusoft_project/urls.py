d222a4974740269c588862eda130560d27e6c81b
chilusoft <mukukchilu@gmail.com>
2023-04-11 01:28:29 +0200

    added logout function to REST API

--- backend/chilusoft_project/urls.py
+++ backend/chilusoft_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
+from custom_auth import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,7 +25,8 @@ urlpatterns = [
     path('api/v1/token/', 
           jwt_views.TokenObtainPairView.as_view(), 
           name ='token_obtain_pair'),
-     path('api/v1/token/refresh/', 
+    path('api/v1/token/refresh/', 
           jwt_views.TokenRefreshView.as_view(), 
-          name ='token_refresh')
+          name ='token_refresh'),
+    path('api/v1/logout/', views.LogoutView.as_view(), name ='logout')
 ]
